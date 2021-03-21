b06d0ef8c3e63c8efd7dfce4378629e7f37656c8
sabjorn <info@sabjorn.net>
2021-03-21 16:41:39 +0100

    get synergy between windowed version and ffmpeg version

--- attractor/src/lib.rs
+++ attractor/src/lib.rs
@@ -4,6 +4,7 @@ pub struct Config
     pub height: usize,
     pub steps: usize,
     pub iterations: u32,
+    pub window_enabled: bool,
     pub a: (f32, f32),
     pub b: (f32, f32),
     pub c: (f32, f32),
@@ -12,20 +13,21 @@ pub struct Config
 
 impl Config {
     pub fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 12 {
+        if args.len() < 13 {
             return Err("not enough arguments");
         }
         let width = args[1].parse::<usize>().unwrap();
         let height = args[2].parse::<usize>().unwrap();
         let steps = args[3].parse::<usize>().unwrap();
         let iterations = args[4].parse::<u32>().unwrap();
+        let window_enabled = args[5].parse::<bool>().unwrap();
 
-        let a = (args[5].parse::<f32>().unwrap(), args[6].parse::<f32>().unwrap()); 
-        let b = (args[7].parse::<f32>().unwrap(), args[8].parse::<f32>().unwrap()); 
-        let c = (args[9].parse::<f32>().unwrap(), args[10].parse::<f32>().unwrap()); 
-        let d = (args[11].parse::<f32>().unwrap(), args[12].parse::<f32>().unwrap()); 
+        let a = (args[6].parse::<f32>().unwrap(), args[7].parse::<f32>().unwrap()); 
+        let b = (args[8].parse::<f32>().unwrap(), args[9].parse::<f32>().unwrap()); 
+        let c = (args[10].parse::<f32>().unwrap(), args[11].parse::<f32>().unwrap()); 
+        let d = (args[12].parse::<f32>().unwrap(), args[13].parse::<f32>().unwrap()); 
         
-        Ok(Config { width, height, steps, iterations, a, b, c, d })
+        Ok(Config { width, height, steps, iterations, window_enabled, a, b, c, d })
     }
 }
 
@@ -41,5 +43,24 @@ impl Points {
         let y = (c*self.x).cos() - (d*self.y).cos();
         self.x = x;
         self.y = y;
+    }
+}
+
+pub struct Colour
+{
+    pub a: u8,
+    pub r: u8,
+    pub g: u8,
+    pub b: u8
+}
+
+impl Colour {
+    fn as_array(&self) -> [u8; 4]
+    {
+        // order expected by minifb
+        [self.b, self.g, self.r, self.a]
+    }
+    pub fn to_u32(&self) -> u32 {
+        u32::from_ne_bytes(self.as_array())
     }
 }
--- attractor/src/main.rs
+++ attractor/src/main.rs
@@ -1,5 +1,5 @@
 // Usage Example
-// WIDTH=1024; HEIGHT=1024; STEPS=600; ITERATIONS=10000; ./target/debug/attractor ${WIDTH} ${HEIGHT} ${STEPS} ${ITERATIONS} -5 -5.5 2 2 2 2 2 2 | ffmpeg -y -f rawvideo -vcodec rawvideo -s ${WIDTH}x${HEIGHT} -pix_fmt rgb24 -r 60 -i - -c:v libx264 -pix_fmt yuv420p -an /tmp/lol.mov
+// WIDTH=1024; HEIGHT=1024; STEPS=600; ITERATIONS=10000; ./target/debug/attractor ${WIDTH} ${HEIGHT} ${STEPS} ${ITERATIONS} -5 -5.5 2 2 2 2 2 2 | ffmpeg -y -f rawvideo -vcodec rawvideo -s ${WIDTH}x${HEIGHT} -pix_fmt bgra -r 60 -i - -c:v libx264 -pix_fmt yuv420p -an /tmp/lol.mov
 extern crate minifb;
 
 use minifb::{Key, Window, WindowOptions};
@@ -8,7 +8,6 @@ use ndarray::prelude::*;
 use std::io::{self, Write};
 use std::mem;
 use std::env;
-
 
 use attractor::{Config};
 
@@ -22,7 +21,9 @@ fn process(canvas: &mut [u32], iterations: u32, a: f32, b:f32, c:f32, d:f32, dim
         let x_pos = ((points.x + 2.) / 4. * ((width - 1) as f32)) as usize;
         let y_pos = ((points.y + 2.) / 4. * ((height - 1) as f32)) as usize;
         let i = x_pos + width * y_pos;
-        canvas[i] = 0xFF0000FF;
+
+        let colour = attractor::Colour{a:255, r:255, g:0, b:0};
+        canvas[i] = colour.to_u32();
     }
 }
 
@@ -35,28 +36,30 @@ fn as_u8_slice(v: &[u32]) -> &[u8] {
     }
 }
 
-fn run(config: Config) {
+fn windowed_processing(canvas: &mut [u32], config: attractor::Config) {
     let a = Array::linspace(config.a.0, config.a.1, config.steps);
     let b = Array::linspace(config.b.0, config.b.1, config.steps);
     let c = Array::linspace(config.c.0, config.c.1, config.steps);
     let d = Array::linspace(config.d.0, config.d.1, config.steps);
-    
-    let mut canvas = vec![0u32; config.width * config.height];
-    
+
     let mut window = Window::new(
         "Test - ESC to exit",
         config.width,
         config.height,
-        WindowOptions::default(),
+        WindowOptions {
+            resize: true,
+            ..WindowOptions::default()
+        },
     ).unwrap_or_else(|e| {
         panic!("{}", e);
     });
 
     // Limit to max ~60 fps update rate
     window.limit_update_rate(Some(std::time::Duration::from_micros(16600)));
+    
     let mut i = 0;
     while window.is_open() && !window.is_key_down(Key::Escape) {
-        process(&mut canvas, config.iterations, a[i], b[i], c[i], d[i], (config.width, config.height));
+        process(canvas, config.iterations, a[i], b[i], c[i], d[i], (config.width, config.height));
         // We unwrap here as we want this code to exit if it fails. Real applications may want to handle this in a different way
         window
             .update_with_buffer(&canvas, config.width, config.height)
@@ -71,30 +74,44 @@ fn run(config: Config) {
         }
         i = (i + 1) % config.steps;
     }
-    // for i in 0..config.steps {
-    //     process(&mut canvas, config.iterations, a[i], b[i], c[i], d[i], (config.width, config.height));
+}
+
+fn run(config: Config) {
+    let mut canvas = vec![0u32; config.width * config.height];
+    
+    if config.window_enabled {
+        windowed_processing(&mut canvas, config);
+        return ()
+    }
+    
+    let a = Array::linspace(config.a.0, config.a.1, config.steps);
+    let b = Array::linspace(config.b.0, config.b.1, config.steps);
+    let c = Array::linspace(config.c.0, config.c.1, config.steps);
+    let d = Array::linspace(config.d.0, config.d.1, config.steps);
+    for i in 0..config.steps {
+        process(&mut canvas, config.iterations, a[i], b[i], c[i], d[i], (config.width, config.height));
         
-    //     let output = as_u8_slice(&canvas);
+        let output = as_u8_slice(&canvas);
 
-    //     io::stdout().write(output).expect("failed to write to stdout");
+        io::stdout().write(output).expect("failed to write to stdout");
 
-    //     unsafe {
-    //       libc::memset(
-    //           canvas.as_mut_ptr() as _,
-    //           0,
-    //           canvas.len() * mem::size_of::<u32>(),
-    //       );
-    //     }
-    // }
+        unsafe {
+          libc::memset(
+              canvas.as_mut_ptr() as _,
+              0,
+              canvas.len() * mem::size_of::<u32>(),
+          );
+        }
+    }
 }
 
 fn main() -> io::Result<()> {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::new(&args).unwrap_or_else(|err| {
-            eprintln!("Problem parsing arguments: {}", err);
-            process::exit(1);
-        });
+        eprintln!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
     run(config);
 
